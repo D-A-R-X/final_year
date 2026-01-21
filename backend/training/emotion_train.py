c78bd5565078479ab0d4c17eaacbf363d53662e3
DARX <surya777dbs@gmail.com>
2026-01-21 12:06:43 +0000

    Improve emotion dataset and retrain model

--- backend/training/emotion_train.py
+++ backend/training/emotion_train.py
@@ -2,6 +2,55 @@ import pandas as pd
 import pickle
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+data = {
+    "text": [
+        # happy
+        "I am happy", "this is great", "awesome work", "feels good",
+        "nice job", "I love this", "so cool", "great experience",
+        "everything is fine", "doing well",
+
+        # sad
+        "I feel sad", "this is depressing", "feeling low", "not feeling good",
+        "I'm tired of this", "so dull", "this hurts", "feels bad",
+        "down today", "not okay",
+
+        # angry
+        "this is annoying", "I am angry", "so frustrating", "this sucks",
+        "I'm mad", "this makes me furious", "can't stand this",
+        "very irritating", "hate this", "pissing me off",
+
+        # neutral
+        "okay", "fine", "hmm", "alright",
+        "not sure", "maybe", "I see", "understood",
+        "got it", "whatever"
+    ],
+    "emotion": (
+        ["happy"] * 10 +
+        ["sad"] * 10 +
+        ["angry"] * 10 +
+        ["neutral"] * 10
+    )
+}
+
+df = pd.DataFrame(data)
+
+model = Pipeline([
+    ("tfidf", TfidfVectorizer()),
+    ("clf", LogisticRegression(max_iter=1000))
+])
+
+model.fit(df["text"], df["emotion"])
+
+with open("models/emotion_model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("✅ Emotion model retrained with expanded dataset")
+import pandas as pd
+import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
 data = {
